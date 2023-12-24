9d21e809fe6b6827bf998f8222bf92e490f9b2e5
Iqbal Hamdani <iqbal.hamdani@koltiva.com>
2023-12-24 16:07:52 +0700

    setup(feat): install bootstrap for utilities and runtime config for key.

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
+    <h1 class="d-flex justify-content-center fs-3 fw-bold text-secondary">Nuxt Koltiva Boilerplate</h1>
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig()
+
+// print /api_secret_token on terminal, and replace a nuxt config runtimeConfig
+console.log(runtimeConfig.apiSecret);
+
+// print /api on console browser
+console.log(runtimeConfig.public.apiBase);
+</script>
+
+<style lang="scss">
+h1 {
+
+}
+</style>
