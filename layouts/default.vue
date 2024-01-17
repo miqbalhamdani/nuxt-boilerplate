e13e91bab1c8aaa99894e55298b5e6448a63d110
Iqbal Hamdani <miqbalh94@gmail.com>
2024-01-17 07:01:56 +0700

    chore(boilerplate): remove bootstrap, revert back page, layout, and manual router

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,5 @@
 <template>
-  <div>
-    <ul class="nav">
-      <li class="nav-item">
-        <NuxtLink
-          class="nav-link active"
-          to="/"
-        >
-          Home
-        </NuxtLink>
-      </li>
-      <li class="nav-item">
-        <NuxtLink
-          class="nav-link"
-          to="/about"
-        >
-          About page
-        </NuxtLink>
-      </li>
-      <li class="nav-item">
-        <NuxtLink
-          class="nav-link"
-          to="/contact"
-        >
-          Contact page
-        </NuxtLink>
-      </li>
-    </ul>
-
-    <div class="container">
-      <NuxtPage />
-    </div>
-  </div>
+  <NuxtPage />
 </template>
 
 <style>
